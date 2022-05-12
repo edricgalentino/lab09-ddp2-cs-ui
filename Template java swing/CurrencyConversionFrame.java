2e1314d54b0270be8dc9c5ac1b94639feee327d4
edricgalentino <erdrikpertama@gmail.com>
2022-05-12 10:34:24 +0700

    asd

--- Template java swing/CurrencyConversionFrame.java
+++ Template java swing/CurrencyConversionFrame.java
@@ -13,63 +13,125 @@ public class CurrencyConversionFrame extends JFrame {
         kurs.put("Euro", 15000);
         kurs.put("US Dollar", 14000);
 
-        // TO DO: Menentukan layout dan size yang ingin digunakan 
-        
+        // TO DO: Menentukan layout dan size yang ingin digunakan
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Currency Converter");
 
         // TO DO: Membuat Panel sesuai kebutuhan
 
-        
+
         labelTitle = new JLabel();
         labelTitle.setText("Welcome to Currency Converter");
+        // make the text bigger and center horizontally but not vertically
+        labelTitle.setFont(labelTitle.getFont().deriveFont(20.0f));
+        labelTitle.setHorizontalAlignment(JLabel.CENTER);
+
+
 
         // TO DO: Membuat dropdown menu
 
 
         labelFrom = new JLabel("From");
-        labelTo = new JLabel("To");
-                
-        // TO DO: Membuat textfield 
+        labelFrom.setText("From");
+        // make the text bigger and center horizontally but not vertically
+        labelFrom.setFont(labelFrom.getFont().deriveFont(20.0f));
 
+        // create a dropdown menu
+        JComboBox<String> from = new JComboBox<String>(currency);
+        // create a text box
+        JTextField fromAmount = new JTextField(10);
+        // set the default value
+        from.setSelectedItem("Rupiah");
+        fromAmount.setText("");
+
+        labelTo = new JLabel("To");
+        // create a dropdown menu
+        JComboBox<String> to = new JComboBox<String>(currency);
+        // create a text box
+        JTextField toAmount = new JTextField(10);
+        // set the default value
+        to.setSelectedItem("US Dollar");
+        toAmount.setText("");
+        // set above to label form
+        labelFrom.setLabelFor(from);
+
+
+        // TO DO: Membuat textfield
 
         convertButton = new JButton("Convert");
         exitButton = new JButton("Exit");
-        
+        // make the button appear in the center of the frame
+        convertButton.setBounds(100, 100, 100, 30);
+        exitButton.setBounds(100, 150, 100, 30);
+        // set text
+        convertButton.setText("Convert");
+        exitButton.setText("Exit");
+        // make both button appear in the same line
+        convertButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
 
 
         // TO DO: Masukkan widget ke panel yang tepat
+        // create a panel for each widget
+        JPanel panelTitle = new JPanel();
+        JPanel panelFrom = new JPanel();
+        JPanel panelTo = new JPanel();
+        JPanel panelButton = new JPanel();
+        // add widget to panel
+        panelTitle.add(labelTitle);
+        panelFrom.add(labelFrom);
+        panelFrom.add(from);
+        panelFrom.add(fromAmount);
+        panelTo.add(labelTo);
+        panelTo.add(to);
+        panelTo.add(toAmount);
+        panelButton.add(convertButton);
+        panelButton.add(exitButton);
+        // add panel to frame
+        add(panelTitle);
+        add(panelFrom);
+        add(panelTo);
+        add(panelButton);
 
-
-        add(labelTitle);
         // TO DO: Masukkan panel ke dalam frame
-
+        // insert panel to frame
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        setSize(300, 300);
 
         setVisible(true);
 
         convertButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                // TO DO: implementasikan pemanggilan method ConvertCurrency
-        
-                
-            }
+            public void actionPerformed(ActionEvent e) {
+                // run method ConvertCurrency with parameter fromAmount.getText() and
+                // get selected item from dropdown menu from
+
+                // set the value of toAmount to the result of ConvertCurrency
+                toAmount.setText(String.valueOf(ConvertCurrency(fromAmount.getText(),
+                        from.getSelectedItem().toString(), to.getSelectedItem().toString())));
+            };
         });
 
         exitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                // TO DO: hentikan eksekusi program
-
+            public void actionPerformed(ActionEvent e) {
+                // close the frame
+                dispose();
             }
         });
     }
-    
+
     // Method converter
-    public String ConvertCurrency(String originalValue, String tipeCurrencyFrom, String tipeCurrencyTo) {
+    public String ConvertCurrency(String originalValue, String tipeCurrencyFrom,
+            String tipeCurrencyTo) {
         double value = Double.valueOf(originalValue);
         double fromValue = value * kurs.get(tipeCurrencyFrom);
         double convertedValue = fromValue / kurs.get(tipeCurrencyTo);
         return String.format("%.2f", convertedValue);
     }
-    
+
+    public static void main(String[] args) {
+        new CurrencyConversionFrame();
+    }
+
 }
